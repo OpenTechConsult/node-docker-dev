1414f7619270c4cc57814c22202e1eca4eac312d
Elom Agboka <opentech.consult@gmail.com>
2023-08-19 06:26:27 +0000

    Working with multiple containers
    Adding a Mongo Container
    Communicating between containers

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const port = process.env.PORT || 3000
+
+main().catch(err => console.log(err))
+
+async function main() {
+  await mongoose.connect('mongodb://sandro:qwerty@mongo:27017/?authSource=admin')
+  console.log("succesfully connected to DB")
+}
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi there FRIEND!!!</h2>')
